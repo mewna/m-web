abcd8d6707feab8edcae10fc1242efa2cb5dc3b7
amy null <null@amy.gg>
2018-07-11 12:41:35 -0400

    Maybe fix PUBLIC_URL

--- src/const.js
+++ src/const.js
@@ -1,16 +1,21 @@
 // https://daveceddia.com/multiple-environments-with-react/
 let backendUrl
+let publicUrl
 
 const hostname = window && window.location && window.location.hostname
 
 if(hostname === "mewna.com") {
     backendUrl = "https://esp.mewna.com"
+    publicUrl = "https://mewna.com/"
 } else if(hostname === "mewna.app") {
     backendUrl = "https://esp.mewna.app"
+    publicUrl = "https://mewna.app/"
 } else {
     backendUrl = process.env.REACT_APP_BACKEND_URL
+    publicUrl = process.env.PUBLIC_URL
 }
 
 export const BACKEND_URL = backendUrl // process.env.REACT_APP_BACKEND_URL
 export const DISCORD_EPOCH = 1420070400000
 export const MEWNA_EPOCH = 1518566400000
+export const PUBLIC_URL = publicUrl
